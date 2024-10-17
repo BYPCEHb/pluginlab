1d9e0b31d7dde906e158cdde6477e93f9e26f2b6
BYPCEHb <467339@niuitmo.ru>
2024-10-17 11:23:16 +0300

    Added new feature for timezone, changed README.

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,28 @@
 import * as vscode from 'vscode';
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.insertDateTime', () => {
-		const now = new Date();
-		const dateTimeString = now.toLocaleString();
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			editor.edit(editBuilder => {
-				editBuilder.insert(editor.selection.active, dateTimeString);
-			});
-		}
-	});
 
-	context.subscriptions.push(disposable);
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.insertDateTime', async () => {
+        const answer = await vscode.window.showInputBox({ prompt: 'Enter the timezone offset (in hours):' });
+        
+        if (answer !== undefined) {
+            const now = new Date();
+            const curTimeZoneOffset = now.getTimezoneOffset() / 60;
+            const wanTimeZone = Number(answer);
+
+            // Рассчитываем новое время
+            const newTime = new Date(now.getTime() + (wanTimeZone + curTimeZoneOffset) * 60 * 1000 * 60);
+            const dateTimeString = newTime.toLocaleString();
+
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+                editor.edit(editBuilder => {
+                    editBuilder.insert(editor.selection.active, dateTimeString);
+                });
+            }
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
